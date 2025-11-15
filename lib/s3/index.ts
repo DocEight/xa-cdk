17e76c944858c9df903a29ed628366d03c376fd7
DocEight <jaholmes1998@gmail.com>
2025-11-15 12:13:13 +0900

    exports

--- lib/s3/index.ts
+++ lib/s3/index.ts
@@ -1,5 +1,8 @@
-export { CrossAccountS3Bucket } from "./bucket";
-export type { CrossAccountS3BucketProps } from "./bucket";
+export { CrossAccountS3Bucket, CrossAccountS3BucketWrapper } from "./bucket";
+export type {
+  CrossAccountS3BucketProps,
+  CrossAccountS3BucketWrapperProps,
+} from "./bucket";
 
 export { CrossAccountS3BucketManager } from "./manager";
 export type { CrossAccountS3BucketManagerProps } from "./manager";
